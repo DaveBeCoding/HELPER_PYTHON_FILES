56ee490b83f29d9742bb43ee59f74ce6164f60c0
davebecoding <d01001010@csu.fullerton.edu>
2022-04-27 14:30:44 -0700

    Python != C++

--- create_file_S/file_creator.py
+++ create_file_S/file_creator.py
@@ -1,16 +1,16 @@
 #!/usr/bin/env python3
 
-# YOU ARE NOT IN C++ LAND!!
+# YOU ARE NOT IN C++ LAND !!
 
-n = int(input("Enter number of elements : "))
+n = int(input("Enter number of File(s) : "))
 
-a = list(map(str, input("\nEnter the numbers : ").strip().split()))[:n]
+a = list(map(str, input("\nEnter File(s) : ").strip().split()))[:n]
 
 for it in a:
     filename = it
     f = open(filename, 'w')
 
-    f.write("something")
+    f.write("print(FILE STAGED)")
     f.close()
 
 print("\nFiles created - ", a)
