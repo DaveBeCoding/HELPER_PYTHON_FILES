9f6476320e04bdebb894334695df60ae742a0f37
davebecoding <d01001010@csu.fullerton.edu>
2022-04-27 13:13:57 -0700

    display args, create files

--- create_file_S/file_creator.py
+++ create_file_S/file_creator.py
@@ -7,30 +7,29 @@ import sys
 
 
 # main.py
-# import sys
 
 if __name__ == "__main__":
     # print(f"Arguments count: {len(sys.argv)}")
     for i, arg in enumerate(sys.argv):
         if sys.argv[0] == arg:
-            continue # turn this into function -> DRIP
+            continue  # turn this into function -> DRIP
         else:
             print(f"Argument {i:>6}: {arg}")
-            i=1
-            arrg_list =['a','b','c','e','f']
+            i = 1
+            arrg_list = ['a', 'b', 'c', 'e', 'f']
             # arrg_list =[]
             # arrg_list.append(sys.argv)
             # print(f'THIS IS LIST {arrg_list}')
-            for it in arrg_list :
-                i_str=str(i)
-                filename=i_str+'.txt'
+            for it in arrg_list:
+                i_str = str(i)
+                filename = i_str+'.txt'
                 # if sys.argv[0] == arg:
                 #     continue
-                f=open( filename,'w')
+                f = open(filename, 'w')
 
                 f.write("something")
                 f.close()
-                i=i+1
+                i = i+1
 
 # SAMPLE INPUT : python file_creator.py  test-01 test-o2 test-03.py test-04 test-5 test-06.c
 
