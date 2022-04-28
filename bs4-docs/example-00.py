7ca8946b4ac4d4d4b09872ea576521d0102ea502
davebecoding <d01001010@csu.fullerton.edu>
2022-04-27 17:27:42 -0700

    testing bs4 methods

--- bs4-docs/example-00.py
+++ bs4-docs/example-00.py
@@ -1,2 +1,50 @@
-def funct(self):
-    pass
+from bs4 import BeautifulSoup
+
+html_doc = """<html><head><title>The Dormouse's story</title></head>
+<body>
+<p class="title"><b>The Dormouse's story</b></p>
+
+<p class="story">Once upon a time there were three little sisters; and their names were
+<a href="http://example.com/elsie" class="sister" id="link1">Elsie</a>,
+<a href="http://example.com/lacie" class="sister" id="link2">Lacie</a> and
+<a href="http://example.com/tillie" class="sister" id="link3">Tillie</a>;
+and they lived at the bottom of a well.</p>
+
+<p class="story">...</p>
+"""
+
+soup = BeautifulSoup(html_doc, 'html.parser')
+
+
+print('----------TITLE-----------------')
+print(soup.title)
+
+print('\n')
+print('------------TITLE-NAME---------------')
+print(soup.title.name)
+
+print('\n')
+print('-----------TITLE-STRING----------------')
+print(soup.title.string)
+
+print('\n')
+print('-------------PARENT-NAME--------------')
+
+print('\n')
+print('-------------<P>--------------')
+
+print('\n')
+print('------------p[class]---------------')
+
+print('\n')
+print('--------------<a>-------------')
+
+
+print('\n')
+print('------------find_all(a)---------------')
+
+print('\n')
+print('------------find(id=link3)---------------')
+
+
+# print(soup.prettify())
