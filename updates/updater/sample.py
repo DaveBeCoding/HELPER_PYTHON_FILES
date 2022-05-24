5d372b169ce446073968d822ad136a8078701f8a
davebecoding <d01001010@csu.fullerton.edu>
2022-05-24 03:07:01 -0700

    updater, updated

--- updates/updater/sample.py
+++ updates/updater/sample.py
@@ -1,22 +1,20 @@
-
+ 
             
             # Python program to
     # demonstrate stack implementation
-    # using collections.deque
-
-    from collections import deque
-
-    stack = deque()
-
+    # using list
+    
+    stack = []
+    
     # append() function to push
     # element in the stack
     stack.append('a')
     stack.append('b')
     stack.append('c')
-
-    print('Initial stack:')
+    
+    print('Initial stack')
     print(stack)
-
+    
     # pop() function to pop
     # element from stack in
     # LIFO order
@@ -25,16 +23,14 @@ Elements popped from stack:')
     print(stack.pop())
     print(stack.pop())
     print(stack.pop())
-
+    
     print('
 Stack after elements are popped:')
     print(stack)
-
+    
     # uncommenting print(stack.pop())
     # will cause an IndexError
     # as the stack is now empty
-
-
             
             
             
