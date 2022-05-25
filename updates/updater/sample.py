b78b3cac42cef03c874dcb3abfc0bfe1a7fb4685
davebecoding <d01001010@csu.fullerton.edu>
2022-05-25 03:07:01 -0700

    updater, updated

--- updates/updater/sample.py
+++ updates/updater/sample.py
@@ -1,31 +1,90 @@
 
-    # Python program to
-    # demonstrate queue implementation
-    # using list
-    
-    # Initializing a queue
-    queue = []
-    
-    # Adding elements to the queue
-    queue.append('a')
-    queue.append('b')
-    queue.append('c')
-    
-    print("Initial queue")
-    print(queue)
-    
-    # Removing elements from the queue
-    print("
-Elements dequeued from queue")
-    print(queue.pop(0))
-    print(queue.pop(0))
-    print(queue.pop(0))
-    
-    print("
-Queue after removing elements")
-    print(queue)
-    
-    # Uncommenting print(queue.pop(0))
-    # will raise and IndexError
-    # as the queue is now empty        
+            # Node class
+    class Node:
+
+        # Function to initialize the node object
+        def __init__(self, data):
+            self.data = data # Assign data
+            self.next = None # Initialize
+                            # next as null
+
+    # Linked List class
+    class LinkedList:
+        
+        # Function to initialize the Linked
+        # List object
+        def __init__(self):
+            self.head = None
+
+    # implement node struct
+
+    # A simple Python program to introduce a linked list
+
+    # Node class
+    class Node:
+
+        # Function to initialise the node object
+        def __init__(self, data):
+            self.data = data # Assign data
+            self.next = None # Initialize next as null
+
+
+    # Linked List class contains a Node object
+    class LinkedList:
+
+        # Function to initialize head
+        def __init__(self):
+            self.head = None
+
+
+    # Code execution starts here
+    if __name__=='__main__':
+
+        # Start with the empty list
+        llist = LinkedList()
+
+        llist.head = Node(1)
+        second = Node(2)
+        third = Node(3)
+
+        '''
+        Three nodes have been created.
+        We have references to these three blocks as head,
+        second and third
+
+        llist.head	 second			 third
+            |			 |				 |
+            |			 |				 |
+        +----+------+	 +----+------+	 +----+------+
+        | 1 | None |	 | 2 | None |	 | 3 | None |
+        +----+------+	 +----+------+	 +----+------+
+        '''
+
+        llist.head.next = second; # Link first node with second
+
+        '''
+        Now next of first Node refers to second. So they
+        both are linked.
+
+        llist.head	 second			 third
+            |			 |				 |
+            |			 |				 |
+        +----+------+	 +----+------+	 +----+------+
+        | 1 | o-------->| 2 | null |	 | 3 | null |
+        +----+------+	 +----+------+	 +----+------+
+        '''
+
+        second.next = third; # Link second node with the third node
+
+        '''
+        Now next of second Node refers to third. So all three
+        nodes are linked.
+
+        llist.head	 second			 third
+            |			 |				 |
+            |			 |				 |
+        +----+------+	 +----+------+	 +----+------+
+        | 1 | o-------->| 2 | o-------->| 3 | null |
+        +----+------+	 +----+------+	 +----+------+
+        '''        
             
