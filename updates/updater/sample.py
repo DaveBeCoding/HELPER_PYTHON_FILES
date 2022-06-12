6e3faa0cbd985ac320985e074728d19b85c5ca15
davebecoding <d01001010@csu.fullerton.edu>
2022-06-12 14:24:01 -0700

    updater, updated

--- updates/updater/sample.py
+++ updates/updater/sample.py
@@ -1,7 +1,24 @@
 
+            # Node class
+    class Node:
 
+        # Function to initialize the node object
+        def __init__(self, data):
+            self.data = data # Assign data
+            self.next = None # Initialize
+                            # next as null
 
-    # A simple Python program for traversal of a linked list
+    # Linked List class
+    class LinkedList:
+        
+        # Function to initialize the Linked
+        # List object
+        def __init__(self):
+            self.head = None
+
+    # implement node struct
+
+    # A simple Python program to introduce a linked list
 
     # Node class
     class Node:
@@ -19,14 +36,6 @@
         def __init__(self):
             self.head = None
 
-        # This function prints contents of linked list
-        # starting from head
-        def printList(self):
-            temp = self.head
-            while (temp):
-                print (temp.data)
-                temp = temp.next
-
 
     # Code execution starts here
     if __name__=='__main__':
@@ -38,9 +47,44 @@
         second = Node(2)
         third = Node(3)
 
+        '''
+        Three nodes have been created.
+        We have references to these three blocks as head,
+        second and third
+
+        llist.head	 second			 third
+            |			 |				 |
+            |			 |				 |
+        +----+------+	 +----+------+	 +----+------+
+        | 1 | None |	 | 2 | None |	 | 3 | None |
+        +----+------+	 +----+------+	 +----+------+
+        '''
+
         llist.head.next = second; # Link first node with second
+
+        '''
+        Now next of first Node refers to second. So they
+        both are linked.
+
+        llist.head	 second			 third
+            |			 |				 |
+            |			 |				 |
+        +----+------+	 +----+------+	 +----+------+
+        | 1 | o-------->| 2 | null |	 | 3 | null |
+        +----+------+	 +----+------+	 +----+------+
+        '''
+
         second.next = third; # Link second node with the third node
 
-        llist.printList()
+        '''
+        Now next of second Node refers to third. So all three
+        nodes are linked.
 
-    
+        llist.head	 second			 third
+            |			 |				 |
+            |			 |				 |
+        +----+------+	 +----+------+	 +----+------+
+        | 1 | o-------->| 2 | o-------->| 3 | null |
+        +----+------+	 +----+------+	 +----+------+
+        '''        
+            
