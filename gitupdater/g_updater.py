08b14ac27096728de16a571890f4fa87b63ecf51
Dave <david00100johnson@gmail.com>
2022-06-22 11:13:37 -0700

    add status bar, and Docstrings for funct(s) ...

--- gitupdater/g_updater.py
+++ gitupdater/g_updater.py
@@ -1,22 +1,33 @@
 # call system wide script
+from ast import For
+from alive_progress import alive_bar
+from time import sleep
 from git import Repo
 from sys import argv
 
-# add a msg to the command line when calling script <-
-# add msg to script var to be used <-
-# msg = ''.join(argv[1:])
-
-# call git add . <-
-# once that msg is available add it to git -m "msg" <-
-# need bytes for double quotes, pure python <-
-
-# call git push <-
-
-MSG = ''.join(argv[1:]) # <- "my commit msg format"
-PATH_OF_GIT_REPO = r'../'  # .git
+MSG = ''.join(argv[1:]) # <- "I like this format"
+PATH_OF_GIT_REPO = r'../'  # .git 
 COMMIT_MESSAGE = MSG
 
-def gitupdater():
+def dotGitExist() -> str:
+    ''' Check for the existance of a .git dir '''
+    pass 
+
+def progressor():
+    '''Terminal status'''
+    with alive_bar(100) as bar:  
+        for i in range(100):
+            sleep(0.03)
+            bar()                       
+
+    # using bubble bar and notes spinner
+    with alive_bar(200, bar = 'bubbles', spinner = 'notes2') as bar:
+        for i in range(200):
+            sleep(0.03)
+            bar()                        
+
+def gitupdater() -> str:
+    '''automate updating repo dir'''
     try:
         repo = Repo(PATH_OF_GIT_REPO)
         repo.git.pull('origin')
@@ -24,7 +35,9 @@ def gitupdater():
         repo.index.commit(COMMIT_MESSAGE)
         origin = repo.remote(name='origin')
         origin.push()
+        progressor()
     except:
         print('Error occured while pushing the code')    
 
 gitupdater()
+
