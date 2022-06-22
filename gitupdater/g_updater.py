a3cd0b5c24d0b3fa8a7a687b3e01a0a1d76ff2ce
dave johnson <dave@daves-MacBook-Pro.local>
2022-06-21 18:32:03 -0700

    setting up user into

--- gitupdater/g_updater.py
+++ gitupdater/g_updater.py
@@ -2,8 +2,13 @@
 
 
 # call system wide script
+from sys import argv
 # add a msg to the command line when calling script
+print(argv[1:])
 # add msg to script var to be used
+msg = ''.join(argv[1:])
+print(msg)
+
 # call git add .
 # once that msg is available add it to git -m "msg"
 # call git push
