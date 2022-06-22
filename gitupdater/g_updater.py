7404479f87f538e69d0f21b63c8b1e24bb08bea4
dave johnson <dave@daves-MacBook-Pro.local>
2022-06-21 17:43:29 -0700

    sketch tool outline for future projects

---
+++ gitupdater/g_updater.py
@@ -0,0 +1,9 @@
+
+
+
+# call system wide script
+# add a msg to the command line when calling script
+# add msg to script var to be used
+# call git add .
+# once that msg is available add it to git -m "msg"
+# call git push
