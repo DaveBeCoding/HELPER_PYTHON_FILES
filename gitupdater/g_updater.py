f356c08201598389a9548765bae48699719675d8
Dave <david00100johnson@gmail.com>
2022-06-22 22:43:11 -0700

    working out <dotGitExist> logic, needs to be updated in <gitupdater> -> Bypass <dotGitExist>

--- gitupdater/g_updater.py
+++ gitupdater/g_updater.py
@@ -1,39 +1,77 @@
+from os import system, path, chdir, walk
 from alive_progress import alive_bar
+from sys import argv, exit
 from time import sleep
-from os import system
-from sys import argv
 from git import Repo
+from vars import *
 
-# maybe create module for var values
-FAIL_EXCEPTION = 'Error occured while pushing the code'
-PATH_OF_GIT_REPO = r'../'  # .git 
-MSG = ''.join(argv[1:]) # <- "I like this format"
-REMOTE_NAME = 'origin'
-COMMIT_MESSAGE = MSG
-SYSTEM_CMD = "clear"
-GIT_PULL = 'origin'
-ONE_HUNDRED = 100
-GIT_ADD = '.'
+# FAIL_EXCEPTION = 'Error occured while pushing the code'
+# PATH_OF_GIT_REPO = r'../'  # .git
+# MSG = ''.join(argv[1:])  # <- "I like this format"
+# REMOTE_NAME = 'origin'
+# COMMIT_MESSAGE = MSG
+# SYSTEM_CMD = "clear"
+# GIT_PULL = 'origin'
+# ONE_HUNDRED = 100
+# GIT_ADD = '.'
+# CWD = './.git'
+# UP_DIR = '../.git'
+# UP_X_TWO = '../../.git'
+# CHANGE_UP = '../'
+# WALK_NEXT = '.'
+# MAX_COUNT = 3
+# COUNT_ONE = 1
+# SLEEPY_TIME = 0.03
 
-def dotGitExist() -> str:
+# logic is a bit off ... 
+# <gitupdater> needs to know where this
+# new location is before being called 
+# Bypass <dotGitExist> -> active (uncalled)
+def dotGitExist() -> bool:
     ''' Check for the existance of a .git dir '''
     # if .git exist call gitupdater
+    count = ONE_HUNDRED - ONE_HUNDRED
+    if count > MAX_COUNT:
+        print("not found")
+        return MAX_COUNT - MAX_COUNT
+    if path.isdir(CWD):
+        print(".git->cwd")
+        # call gitupdater
+        # gitupdater()
+    else:
+        if path.isdir(UP_DIR):
+            new_location = chdir(CHANGE_UP)
+            print('.git -> up one dir' + str(new_location))
+            # call gitupdater
+            # gitupdater()
+        elif path.isdir(UP_X_TWO):
+            print('.git -> up two dir')
+            # call gitupdater
+            # gitupdater()
+        else:
+            dir = next(walk(WALK_NEXT))[COUNT_ONE]
+            dir_str = ''.join(dir)
+            chdir('./'+dir_str)
+            count += COUNT_ONE
+            dotGitExist()
+
     # else, find root dir to check if .git exist
-    pass 
+
 
 def progressor():
     '''Terminal status'''
-    with alive_bar(ONE_HUNDRED) as bar:  
+    with alive_bar(ONE_HUNDRED) as bar:
         for i in range(ONE_HUNDRED):
-            sleep(0.03)
-            bar()                       
+            sleep(SLEEPY_TIME)
+            bar()
 
     # using bubble bar and notes spinner
-    with alive_bar(ONE_HUNDRED + ONE_HUNDRED, bar = 'bubbles', 
-                        spinner = 'notes2') as bar:
+    with alive_bar(ONE_HUNDRED + ONE_HUNDRED, bar='bubbles',
+                   spinner='notes2') as bar:
         for i in range(ONE_HUNDRED + ONE_HUNDRED):
-            sleep(0.03)
-            bar()                        
+            sleep(SLEEPY_TIME)
+            bar()
+
 
 def gitupdater() -> str:
     '''automate updating repo dir'''
@@ -49,12 +87,18 @@ def gitupdater() -> str:
     except:
         print(FAIL_EXCEPTION)
 
+
 def main():
+    '''Main, call functions to perform github automation .:. Example [g_updater.py "write git message"...]'''
+    if len(MSG) < COUNT_ONE:
+        input('example input -> g_updater.py "your message to github"')
+        exit()
     system(SYSTEM_CMD)
     # call progressor
     progressor()
     # call does dotgitexist
+    # dotGitExist()
     gitupdater()
 
-main()
 
+main()
---
+++ gitupdater/vars.py
@@ -0,0 +1,19 @@
+from sys import argv
+
+FAIL_EXCEPTION = 'Error occured while pushing the code'
+PATH_OF_GIT_REPO = r'../'  # .git
+MSG = ''.join(argv[1:])  # <- "I like this format"
+REMOTE_NAME = 'origin'
+COMMIT_MESSAGE = MSG
+SYSTEM_CMD = "clear"
+GIT_PULL = 'origin'
+ONE_HUNDRED = 100
+GIT_ADD = '.'
+CWD = './.git'
+UP_DIR = '../.git'
+UP_X_TWO = '../../.git'
+CHANGE_UP = '../'
+WALK_NEXT = '.'
+MAX_COUNT = 3
+COUNT_ONE = 1
+SLEEPY_TIME = 0.03
