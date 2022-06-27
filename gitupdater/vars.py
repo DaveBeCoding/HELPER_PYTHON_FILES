8e0cfb03075c63a080aa32c34d59e2b3e7886be2
Dave <david00100johnson@gmail.com>
2022-06-26 17:43:20 -0700

    updating main script terminal tool gitupdater -> universal dir test 1 passed

--- gitupdater/vars.py
+++ gitupdater/vars.py
@@ -1,19 +1,34 @@
 from sys import argv
+from os import getcwd
 
-FAIL_EXCEPTION = 'Error occured while pushing the code'
-PATH_OF_GIT_REPO = r'../'  # .git
-MSG = ''.join(argv[1:])  # <- "I like this format"
-REMOTE_NAME = 'origin'
-COMMIT_MESSAGE = MSG
-SYSTEM_CMD = "clear"
-GIT_PULL = 'origin'
-ONE_HUNDRED = 100
+# GIT VARs
 GIT_ADD = '.'
-CWD = './.git'
+MSG = ''.join(argv[1:])  # <- "I like this format"
+COMMIT_MESSAGE = MSG
+GIT_PULL = 'origin'
+REMOTE_NAME = 'origin'
+FAIL_EXCEPTION = 'Error occured while pushing the code'
+
+# SYSTEM
 UP_DIR = '../.git'
-UP_X_TWO = '../../.git'
+SYSTEM_CLEAR = "clear"
+CWD = getcwd() + '/.git'
+UP_DIR_X_TWO = '../../.git'
+
 CHANGE_UP = '../'
-WALK_NEXT = '.'
-MAX_COUNT = 3
+CHANGE_UP_SQUARE = '../../'
+
+# COUNTERS
+ONE_HUNDRED = 100
 COUNT_ONE = 1
+MAX_COUNT = 3
 SLEEPY_TIME = 0.03
+EXIT_CODE = 0
+
+# MAIN
+WALK_NEXT = '.'
+PATH_OF_GIT_REPO = r'../'  # .git <- remove modulerized
+
+
+
+
