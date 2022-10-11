4a85ea66707c87b25f97664872d644ac55d926c6
dave <djohn10@sandia.gov>
2022-10-11 13:39:54 -0600

    Multi Regression example

--- ML_Stuff/Multiple_Regression/multi_regression_00.py
+++ ML_Stuff/Multiple_Regression/multi_regression_00.py
@@ -1,4 +1,7 @@
 import pandas
+from sklearn import linear_model # Linear regression object -> has a method call "fit()"
+#Fit: takes the independent and dependent values as parameters and fills the regression object with data
+# that describes the relationship 
 
 # predict the CO2 emission of a car based on the size of the engine
 # with multiple regression we can throw in more variables, 
@@ -17,6 +20,12 @@ y = df['CO2']
 # NOTE: common to name the list of independent values with a upper case X, 
 # .... and the list of dependent values with a lower case y
 
+# 
+regr = linear_model.LinearRegression()
+regr.fit(X, y)
+
+#Predict the CO2 emission of a car where the weight is 2300(kg), and the volume is 1300(cm3)
+predictedCO2 = regr.predict([[2300, 1300]])
 
 
 
