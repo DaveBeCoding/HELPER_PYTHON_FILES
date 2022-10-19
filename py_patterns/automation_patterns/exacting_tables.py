b4f13f9dbf119bd8af06de3ee60b8037b19e50b2
dave <djohn10@sandia.gov>
2022-10-19 11:42:30 -0600

    automation pattern update(s)

--- py_patterns/automation_patterns/exacting_tables.py
+++ py_patterns/automation_patterns/exacting_tables.py
@@ -2,4 +2,6 @@ import pandas as pd
 
 seasons_1_20 = pd.read_html('https://en.wikipedia.org/wiki/List_of_The_Simpsons_episodes_(seasons_1%E2%80%9320)#Season_1_(1989%E2%80%9390)')
 
-print(len(seasons_1_20)) 
+print(len(seasons_1_20))
+
+print(seasons_1_20[1])
